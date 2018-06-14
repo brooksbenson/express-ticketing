1cce7b015ca2f275945edc7fe065641683d9bbd7
Brooks Benson <brooksbenson01@gmail.com>
2018-06-13 22:30:40 -0600

    TicketControl tested

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 export default props => (
-  <div>
+  <div className={props.className}>
     <h3> Ticket Control </h3>
     <div>
       <h4> Triage </h4>
       <span> Urgency </span>
       <select
         className="select"
-        onChange={this.onUrgencyChange}
-        value={urgency}
+        onChange={e => props.onUrgencyChange(e.target.value)}
+        value={props.urgency}
       >
         <option value="low">Low</option>
         <option value="medium">Medium</option>
         <option value="high">High</option>
       </select>
+      <button onClick={props.onStatusChange}>
+        {props.isOpen ? 'Close Ticket' : 'Reopen Ticket'}
+      </button>
     </div>
     <div>
       <h4> Users </h4>
-      <button> Add </button>
-      <ul>{ticketUsers.map(u => <li key={u.key}> {u.name} </li>)}</ul>
+      <button onClick={props.openAddUserModal}> Add </button>
+      <ul>{props.users.map(u => <li key={u.key}>{u.name}</li>)}</ul>
     </div>
   </div>
 );
--- src/tests/components/TicketControl.test.js
+++ src/tests/components/TicketControl.test.js
@@ -6,7 +6,9 @@ import TicketControl from '../../components/TicketControl';
 
 const { userKeys, urgency } = ticketsArr[2];
 
-const users = userKeys.map(key => usersObj[key]);
+const users = userKeys.map(key => ({ ...usersObj[key], key }));
+const className = 'ticket-control';
+const isOpen = true;
 
 let onUrgencyChange;
 let onStatusChange;
@@ -19,10 +21,67 @@ beforeEach(() => {
   openAddUserModal = jest.fn();
   wrapper = shallow(
     <TicketControl
+      className={className}
+      isOpen={isOpen}
       users={users}
       onUrgencyChange={onUrgencyChange}
       onStatusChange={onStatusChange}
       openAddUserModal={openAddUserModal}
     />
   );
+});
+
+test('TicketControl should match snapshot', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('TicketControl should give wrapper element the className passed as a prop', () => {
+  expect(wrapper.hasClass(className)).toBeTruthy();
+});
+
+test('TicketControl should invoke onUrgencyChange on urgency change', () => {
+  const value = 'high';
+  wrapper.find('select').simulate('change', { target: { value } });
+  expect(onUrgencyChange).toHaveBeenCalledWith(value);
+});
+
+test('TicketControl should render a list of users correctly', () => {
+  const userListItems = wrapper.find('li');
+  expect(userListItems.at(0).text()).toBe(users[0].name);
+  expect(userListItems.at(1).text()).toBe(users[1].name);
+});
+
+test('TicketControl should invoke onStatusChange on close ticket button click', () => {
+  wrapper
+    .find('button')
+    .at(0)
+    .simulate('click');
+  expect(onStatusChange).toHaveBeenCalled();
+});
+
+test('TicketControl should invoke openAddUserModal on add user button click', () => {
+  wrapper
+    .find('button')
+    .at(1)
+    .simulate('click');
+  expect(openAddUserModal).toHaveBeenCalled();
+});
+
+test('TicketControl should render ticket status button with text "Close Ticket" when isOpen prop is true', () => {
+  expect(
+    wrapper
+      .find('button')
+      .at(0)
+      .text()
+  ).toBe('Close Ticket');
+});
+
+test('TicketControl should render ticket status button with text "Reopen Ticket" when isOpen prop is false', () => {
+  wrapper.setProps({ isOpen: false });
+  expect(
+    wrapper
+      .find('button')
+      .at(0)
+      .text()
+  ).toBe('Reopen Ticket');
 });
